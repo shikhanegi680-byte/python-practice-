7f55f05a78f1cc29335504985a36455d8fc558af
shikhanegi680-byte <shikhanegi680@gmail.com>
2026-08-03 22:31:01 +0530

    Create boolean_logic.py

---
+++ boolean_logic.py
@@ -0,0 +1,6 @@
+is_internet_connected = true
+is_battery_low = false 
+if is_internet_connected and not is battery_low:
+    print("perfect! you can continue coding tonight.")
+else:
+  print("please check your network or plug in your charger.")
