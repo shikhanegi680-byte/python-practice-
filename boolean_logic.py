81d5c7fe9500bace49cf9253a56e76ef88fec05c
Shikha Negi <shikhanegi680@gmail.com>
2026-08-04 15:58:43 +0530

    Update boolean_logic.py

--- boolean_logic.py
+++ boolean_logic.py
@@ -1,6 +1,8 @@
-is_internet_connected = true
-is_battery_low = false 
-if is_internet_connected and not is battery_low:
+is_internet_connected = True
+is_battery_low = False
+
+if is_internet_connected and not is_battery_low:
     print("perfect! you can continue coding tonight.")
 else:
-  print("please check your network or plug in your charger.")
+    print("please check your network or plug in your charger.")
+
