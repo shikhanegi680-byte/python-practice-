95ac13560ad43656afc0ccce4d87dcd31e9c559b
Shikha Negi <shikhanegi680@gmail.com>
2026-08-04 15:37:10 +0530

    Update my_skills_list.py

--- my_skills_list.py
+++ my_skills_list.py
@@ -1,4 +1,5 @@
-  skills = ["python,"github", "VS code", "data science","DSA"]
-  print(skills I am learning foe BCA AI & DS:")
-      for skill in skill:
-        print("_" + skill)
+skills = ["python", "github", "VS code", "data science", "DSA"]
+print("skills I am learning for BCA AI & DS:")
+for skill in skills:
+    print(" " + skill)
+
