832983cab3267e1e32091e90cb9eec5f98906296
Shikha Negi <shikhanegi680@gmail.com>
2026-08-04 15:41:16 +0530

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,4 +1,6 @@
-name = input("shikha")
-age = input("17")
-print("namaste" + name + "!")
-print("aap" + age + "saal ke hain or coding sikh rahe hain|")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print(f"namaste {name} !")
+print(f"aap {age} saal ke hain or coding sikh rahe hain!")
+
