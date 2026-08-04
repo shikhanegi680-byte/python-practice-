fdfc124a84cb0ab1f96d42b12c152ccc4799ddab
Shikha Negi <shikhanegi680@gmail.com>
2026-08-04 15:39:17 +0530

    Update voting_check.py

--- voting_check.py
+++ voting_check.py
@@ -1,5 +1,6 @@
-age = int(input("enter your age:")
-          if age >= 18:
-             print("you are eligible to vote")
-          else;
-             print("you are not eligible to vote yet")
+age = int(input("enter your age: "))
+if age >= 18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote yet")
+
