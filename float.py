33fcbd542b27b79b212e0578df61e254eecae8af
Shikha Negi <shikhanegi680@gmail.com>
2026-09-18 20:53:45 +0530

    Create float.py

---
+++ float.py
@@ -0,0 +1,14 @@
+name = "shikha"
+age = 18
+print(type(age))
+print(type(name))
+
+a = int(input("enter a num"))
+b = int(input("enter a num"))
+sum = a + b
+print(sum)
+
+a = flaot(input("enter a num")
+b = float(input("enter a num")
+print("avg = ", a + b/2)
+          
